568e3e62bdcd44402e576380d64b9a44cf857f79
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-23 15:18:15 -0700

    create knex.js, knexfile.js, and first migration

---
+++ migrations/20170623150131_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name', 50).notNullable().defaultTo('');
+    table.string('last_name', 50).notNullable().defaultTo('');
+    table.string('email', 100).notNullable().unique();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamps(true, true);
+  })
+};
+
+// NOTE: do we want to change data type for password?
+      // do we want timestamps for each table? (probably)
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
