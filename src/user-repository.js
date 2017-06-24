09178d5b8a7e57c0dff04e7142481cd53ba2ca86
Grant Willison <emailgdw@gmail.com>
2017-06-24 10:12:32 -0700

    untested route work on users.js

---
+++ src/user-repository.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const knex = require('../knex');
+
+class UserRepository{
+  checkIfUserIsRegistered(hash){
+    return knex('users')
+      .where({hashed_password: hash})
+      .first()
+
+  }
+// POST-4) receive email - check against db
+  authenticate(email){
+    // POST-5) get array of objects from knex - return id, db_hashed_pw
+    return knex('users')
+      .select('id', 'first_name', 'last_name', 'email', 'hashed_password')
+      .where({email: email})
+      .first();
+  }
+
+
+  register(body, hash){
+    return knex.insert({first_name:body.first_name, last_name: body.last_name, email: body.email, hashed_password: hash}, 'id')
+      .into('users')
+      .then((ids) => {
+        return knex('users')
+            .select('id', 'first_name', 'last_name', 'email')
+            .where('id',ids[0]);
+    });
+  }
+
+}
+
+module.exports = UserRepository;
