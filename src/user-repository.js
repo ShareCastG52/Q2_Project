87e179a36d03306895542ac55a1384620829ba78
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-27 12:02:03 -0700

    Resolve merge conflict

--- src/user-repository.js
+++ src/user-repository.js
@@ -3,21 +3,12 @@
 const knex = require('../knex');
 
 class UserRepository{
-<<<<<<< HEAD
-=======
-  checkIfUserIsRegistered(hash){
-    return knex('users')
-      .where({hashed_password: hash})
-      .first()
-
-  }
->>>>>>> fce494d4c3d466c987173411f3c6b8b9c824b1f4
+  
 // POST-4) receive email - check against db
   authenticate(email){
     // POST-5) get array of objects from knex - return id, db_hashed_pw
     return knex('users')
       .select('id', 'first_name', 'last_name', 'email', 'hashed_password')
-<<<<<<< HEAD
       .where({ email: email})
       .first();
   }
@@ -28,13 +19,7 @@ class UserRepository{
     .first()
 
   }
-=======
-      .where({email: email})
-      .first();
-  }
 
-
->>>>>>> fce494d4c3d466c987173411f3c6b8b9c824b1f4
   register(body, hash){
     return knex.insert({first_name:body.first_name, last_name: body.last_name, email: body.email, hashed_password: hash}, 'id')
       .into('users')
