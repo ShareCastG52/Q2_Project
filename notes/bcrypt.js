d07a120419f66d6bebb76a59a01f5d69997c6d8f
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-27 11:52:28 -0700

    Fix merge conflict

--- notes/bcrypt.js
+++ notes/bcrypt.js
@@ -1,7 +1,6 @@
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-<<<<<<< HEAD
 bcrypt.hash('youreawizard', saltRounds).then(function(hash) {
     console.log(hash);
 });
@@ -9,12 +8,4 @@ bcrypt.hash('youreawizard', saltRounds).then(function(hash) {
 // bcrypt.compare(myPlaintextPassword, hash).then(function(res) {
 //     // res == true
 // });
-=======
-bcrypt.hash('Dummy', saltRounds).then(function(hash) {
-    console.log(hash);
-});
 
-bcrypt.compare(myPlaintextPassword, hash).then(function(res) {
-    // res == true
-});
->>>>>>> fce494d4c3d466c987173411f3c6b8b9c824b1f4
