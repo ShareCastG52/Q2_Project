3a36142478a6fdf150ae96db5121b276bac6d639
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-29 15:13:31 -0700

    add apidoc folder to github

---
+++ apidoc/doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-06-28T16:11:31.026Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
