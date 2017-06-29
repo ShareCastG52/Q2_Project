076274cd0bd3fa0e827c42f2e3137dcec1e16fd5
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-28 18:02:14 -0700

    minor changes for push

--- routes/search.js
+++ routes/search.js
@@ -29,7 +29,7 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.post('/', (req, res, next) => {
+router.post('/', checkForToken, verifyUser, (req, res, next) => {
   let podcastItunesId = req.body.collectionId;
   let userId = getUserId(req);
   let podcastData;
